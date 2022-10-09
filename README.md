980dcdd3d48fa7c2cf4575dd158baf00d6385d59
sanjanap17 <sanjanap@usc.edu>
2022-10-08 17:30:57 -0700

    Problem 1 and Problem 2 done

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # multi-drone-quiz
+## Problem #1
+ESDF calculation using Image Foresting Transform
+Obstacles are considered as roots of the tree and their distance values are set to 0. Propogate outwards from the root to the neighboring cells and compute Euclidean distance.
+
+Reference: (https://medium.com/on-coding/euclidean-distance-transform-d37e06958216)
+
+## Problem #2
+Assumption 1: The height of the drone above the ground is known.
+Assumption 2: At a discrete time step, the drone is over one cell of the grid.
+
+Consider a grid of possible locations that the drone can fly over at the discrete time intervals. Since the obstacles do not move, create a map using ESDF as per Problem #1. When the drone is above a particular cell of the grid, the cell stores the horizontal distance to the nearest obstacle and the vertical distance of the drone is known (Assumption).  Using Pythagoras theorem, the distance between the drone and obstacle can be computed. If the obstacles and drone are in the same horizontal plane, this calculation is not needed and the distance to nearest obstacle is directly obtained from the ESDF grid.
+ESDF needs to be computed only if obstacles move. As they do not move, it can be calculated once. When the drones move, a simple calculation of the distance needs to be calculated.
+
+## Problem #3
+
+## Problem #4
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import time
 from result import *
+import math
+import numpy as np
+import heapq
 
+def valid(x,y,M,N):
+    if ((x<M and 0<=x) and (0<=y and y<N)):
+        return True
+    else:
+        return False
+
+def remove(q,indices):
+    try:
+        q.remove(indices)
+    except ValueError:
+        pass
 
 def esdf(M, N, obstacle_list):
     """
@@ -9,6 +23,37 @@ def esdf(M, N, obstacle_list):
     :param obstacle_list: Obstacle list
     :return: An array. The value of each cell means the closest distance to the obstacle
     """
+    distance = [[2**16 for y in range(N)] for x in range(M)] 
+    roots = [[2**16 for y in range(N)] for x in range(M)] 
+    q = []
+    eight_neighbor_adj =[[1,0],[0,1],[1,1],[-1,0],[0,-1],[-1,-1],[-1,1],[1,-1]]
+    for i in range(len(obstacle_list)):
+        obs = obstacle_list[i]
+        distance[obs[0]][obs[1]] = 0
+        roots[obs[0]][obs[1]] = obs
+        heapq.heappush(q, obs)
+    
+    while (len(q) != 0):
+        curr = heapq.heappop(q)
+        for i in eight_neighbor_adj:
+            x = curr[0] + i[0]
+            y = curr[1] + i[1]
+            indices = []
+            indices.append(x)
+            indices.append(y)
+            if valid(x,y,M,N):
+                obstacle = roots[curr[0]][curr[1]]
+                curr_dist = math.sqrt((indices[0]-obstacle[0])**2 + (indices[1]-obstacle[1])**2)
+                if curr_dist < distance[indices[0]][indices[1]]:
+                    if distance[indices[0]][indices[1]] != 2^16:
+                        remove(q,indices)
+                    if curr_dist.is_integer():
+                        curr_dist = int(curr_dist)
+                    distance[x][y] = (curr_dist)
+                    roots[x][y] = obstacle
+                    heapq.heappush(q,indices)
+    return(distance)
+   
     pass
 
 
